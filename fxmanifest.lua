6f3e7576d78f1a9da0017e82beff911ba65c8a59
N0tZetsu <emamainn@gmail.com>
2025-04-16 22:19:17 -0300

    Add fxmanifest.lua 1744852756705

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BlackboxAI'
+description 'ESX Custom HUD with Voice Activity'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/main.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js',
+    'html/img/*.png'
+}
+
+dependencies {
+    'es_extended'
+}
